d06aa90490c05b335f6ee5766c93cf39b473d231
CMCCVR <99026597+CMCCVR@users.noreply.github.com>
2022-08-03 21:54:45 -0300

    loops 03

--- loops.js
+++ loops.js
@@ -1,3 +1,7 @@
 let estados = ['Ceará', 'Goiás', 'CABO', 'Maranhao'];
 
-estados.forEach(estado => console.log(estado))
+// o objeto estados é uma Array, desta forma ele possui um atributo
+// chamado length (tamanho), desta forma ele passa para a função forEach,
+// que sabe que ele vai passsar por cada
+// elemento ate o ultimo, acessado atrave da propriedade length.
+estados.forEach((estado, indice) => console.log(indice, estado))
